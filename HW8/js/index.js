6d6a4e2bffc5647c6c5873f4d07cee257d7507f6
gaponow <gaponovv19@gmail.com>
2019-01-19 21:15:57 +0200

    changed indents

--- HW8/js/index.js
+++ HW8/js/index.js
@@ -4,71 +4,70 @@ function gallery(el) {
     const images = el.querySelectorAll('[data-img]');
     const elements = el.querySelectorAll('[data-elm]');
     let index = 0,
-        index2 = 0;
+    index2 = 0;
 
     function addClass(content) {
-		if(elements[index + content].classList.contains(`elm${i + 1}`)) {
-		elements[index + content].classList.add(`elm${i + 1}_first`);
-		}
+        if(elements[index + content].classList.contains(`elm${i + 1}`)) {
+            elements[index + content].classList.add(`elm${i + 1}_first`);
+        }
     };
 
     function removeClass(content) {
         if(elements[index + content].classList.contains(`elm${i + 1}`)) {
-        elements[index + content].classList.remove(`elm${i + 1}_first`);
+            elements[index + content].classList.remove(`elm${i + 1}_first`);
         }
     };
 
     function prevElm() {
         elements[index].classList.remove('active');
         index--;
-		for(i = 0; i < elements.length; i++) {
-			removeClass(1);
-		}
+        for(i = 0; i < elements.length; i++) {
+            removeClass(1);
+    }
 
-        if(index < 0) {
-			for(i = 0; i < elements.length; i++) {
-				addClass(i + 1);
-			}
-            index = elements.length - 1;
+    if(index < 0) {
+        for(i = 0; i < elements.length; i++) {
+            addClass(i + 1);
         }
+        index = elements.length - 1;
+    }
 
-        elements[index].classList.add('active');
+    elements[index].classList.add('active');
     };
 
     function nextElm() {
         elements[index].classList.remove('active');
         index++;
         for(i = 0; i < elements.length; i++) {
-			addClass(- 1);
-		}
+            addClass(- 1);
+        }
         if(index >= elements.length) {
             index = 0;
             for(i = 0; i < elements.length; i++) {
-				removeClass(i);
-			}
+                removeClass(i);
         }
+    }
 
-        elements[index].classList.add('active');
+    elements[index].classList.add('active');
     };
 
     function prevImg() {
-      images[index2].classList.remove('showed');
-      index2--;
-      
-      if(index2 < 0) {
-        index2 = images.length - 1;
-      }
-      images[index2].classList.add('showed');
+        images[index2].classList.remove('showed');
+        index2--;
+        if(index2 < 0) {
+            index2 = images.length - 1;
+        }
+        images[index2].classList.add('showed');
     };
 
     function nextImg() {
-      images[index2].classList.remove('showed');
-      index2++;
-      
-      if(index2 >= images.length) {
-        index2 = 0;
-      }
-      images[index2].classList.add('showed');
+        images[index2].classList.remove('showed');
+        index2++;
+
+        if(index2 >= images.length) {
+            index2 = 0;
+        }
+        images[index2].classList.add('showed');
     };
 
     function prevObj() {
@@ -83,16 +82,16 @@ function gallery(el) {
 
     prevBtn.addEventListener('click', prevObj);
     nextBtn.addEventListener('click', nextObj);
-	
-	window.addEventListener('keydown', function (e) {
-		if (e.keyCode == 37) {
-			prevObj();
-		}
-		if (e.keyCode == 39) {
-			nextObj();
-		}
-	});
-	
+
+    window.addEventListener('keydown', function (e) {
+        if (e.keyCode == 37) {
+            prevObj();
+        }
+        if (e.keyCode == 39) {
+            nextObj();
+        }
+    });
+
 };
 
 gallery(document.querySelector('[data-gallery]'))
